b7e771bfbc441f48313c3ad400b7e022d1f950f9
이동아 <christar99@naver.com>
2024-10-21 17:54:01 +0900

    useProduct hook 구현

--- src/refactoring/hooks/use-product.ts
+++ src/refactoring/hooks/use-product.ts
@@ -2,9 +2,27 @@ import { useState } from "react";
 import { ProductType } from "../../types";
 
 export const useProductList = (initialProductList: ProductType[]) => {
+  const [productList, setProductList] = useState(initialProductList);
+
+  function updateProduct(productItem: ProductType) {
+    setProductList(prev => getFormatProduct(prev, productItem));
+  }
+
+  function addProduct(newProduct: ProductType) {
+    setProductList(prev => addNewProduct(prev, newProduct));
+  }
+
+  const getFormatProduct = (prev: ProductType[], productItem: ProductType) => {
+    return prev.map(product => (product.id === productItem.id ? productItem : product));
+  };
+
+  const addNewProduct = (prev: ProductType[], newProduct: ProductType) => {
+    return [...prev, newProduct];
+  };
+
   return {
-    productList: initialProductList,
-    updateProduct: () => undefined,
-    addProduct: () => undefined,
+    productList,
+    updateProduct,
+    addProduct,
   };
 };
